e37f23fc460ce2a28756cba730983f99247e9315
Juliette Monsel <j_4321@protonmail.com>
2017-08-30 10:51:25 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='tkcolorpicker',
-    version='2.1.0',
+    version='2.1.1',
     description='Color picker dialog for Tkinter',
     long_description=long_description,
     url='https://github.com/j4321/tkColorPicker',
